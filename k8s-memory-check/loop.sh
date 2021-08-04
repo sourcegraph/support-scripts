a255b48780fda88112b9a154f2f878cdbf620a25
Dax McDonald <31839142+daxmc99@users.noreply.github.com>
2021-08-04 12:15:20 -0700

    shfmt

--- k8s-memory-check/loop.sh
+++ k8s-memory-check/loop.sh
@@ -2,9 +2,8 @@
 
 # check memory usage of gitserver every 5 min until the process is killed.
 
-while true;
-do
-    echo "Time Now: $(date +%H:%M:%S)"
-    kubectl top pods | grep gitserver
-    sleep 3600 #every 5 minute
+while true; do
+	echo "Time Now: $(date +%H:%M:%S)"
+	kubectl top pods | grep gitserver
+	sleep 3600 #every 5 minute
 done
